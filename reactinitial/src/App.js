b16727b5905f7e2d24a78a338f90a2e0f32fbdcb
ribaorsi <ribaorsi@gmail.com>
2022-04-26 13:16:07 +0200

    2 select added

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -1,10 +1,45 @@
 import React, { useEffect, useState } from "react"
+import { Button, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 const App = () => {
 
   return (
     <div>
-      Todo...
+      <FormControl fullWidth>
+  <InputLabel id="Color">Choose your lightsaber</InputLabel>
+  <Select
+    labelId="lightsaber"
+    id="lightsaber"
+    value="lightsaber"
+    label="Lightsaber"
+    
+  >
+    <MenuItem value="Blue">Blue</MenuItem>
+    <MenuItem value="Green">green</MenuItem>
+    <MenuItem value="Red">Red</MenuItem>
+  </Select>
+
+  <InputLabel id="SithorJedi">Sith or Jedi?</InputLabel>
+  <Select
+    labelId="lightsaber"
+    id="lightsaber"
+    value="lightsaber"
+    label="Lightsaber"
+    
+  >
+    <MenuItem value="Sith">Sith</MenuItem>
+    <MenuItem value="Jedi">Jedi</MenuItem>
+  </Select>
+
+  
+</FormControl>
+      
+       
+          
+     
+      
+<Button variant="contained">Küldés</Button>
+
     </div>
   )
 }
